c6b8b24dd6d6718a18f2c5fd06c98c58ace4d6b6
Jamon Dixon <jamonadixon@gmail.com>
2021-01-21 20:59:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   string_to_array = string.split("")
 
   string_to_array.collect do |element|
-    
+
     new_reverse_array << element.reverse
 end
 new_reverse_array.join("")
