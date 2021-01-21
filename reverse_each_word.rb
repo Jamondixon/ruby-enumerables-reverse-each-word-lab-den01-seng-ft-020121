8638c7d8443647252e8b255aea0e89ba475b1ca0
Jamon Dixon <jamonadixon@gmail.com>
2021-01-21 21:03:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,8 @@
 def reverse_each_word(string)
   string_to_array = string.split("")
   new_reverse_array = []
-  string_to_array.collect do |element|
+  string_to_array.collect do |element| element.reverse
 
-    new_reverse_array << element.reverse!
 end
 new_reverse_array.join("")
 end
